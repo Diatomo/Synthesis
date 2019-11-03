badaf6e37465bd369bdf1d274c9df7f39179eca2
Diatomo <Wojtekenson@gmail.com>
2019-11-03 17:42:19 -0500

    Added, permutation to randomizer featurewq`

--- parser.py
+++ parser.py
@@ -1,16 +1,28 @@
 
 
-#TODO test incomplete file names
-class Parser:
+'''
+    Class: Parser
+    Description:
+        This takes a file and parses it into different sections
+
+'''
+class Sequence(object):
 
     def __init__(self):
-       self.song = {}
-       self.f = ''
+        self.song = {}
+        self.notes = ['C','C#', 'D', 'D#', 'E','F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
     def printSong(self):
-        for key, value in self.song.items():
+        for key, value, in self.song.items():
             print(key)
             print(value)
+
+#TODO test incomplete file names
+class Parser(Sequence):
+
+    def __init__(self):
+        super(Sequence, self).__init__()
+        self.f = ''
 
     def loadFile(self, f):
         self.f = open(f,'r+')
@@ -44,25 +56,28 @@ class Parser:
             println("Incorrect format")
 
 
-class Randomizer:
+'''
+    Class: Randomizer
+    Description:
+        Produces random sequences from permutation
+        or just randomly producing a string of notes
+'''
+class Randomizer(Sequence):
 
     def __init__(self):
+        super(Sequence, self).__init__()
         self.length = 32
         self.octaveMin = 2
         self.octaveMax = 6
-        self.notes = ['A','B','C','D','E','F','G']
         self.octaves = range(self.octaveMin, self.octaveMax)
-        self.song = {}
-        self.notes = []
-
+        self.allBars = []
+        self.notes = ['C','C#', 'D', 'D#', 'E','F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
     def printNotes(self):
         print(self.notes)
 
-    def printSong(self):
-        for key, value, in self.song.items():
-            print(key)
-            print(value)
+    def printAllBars(self):
+        print(self.allBars)
 
     def changeLength(self, length):
         self.length = length
@@ -70,11 +85,29 @@ class Randomizer:
     def setScale(self,scale):
         pass
 
+    def random(self, length):
+        tempSection = []
+        if (len(self.allBars) == 0):
+            for (bar in range(length)):
+                tempSection.append(self.allBars(randrange(0,len(allBars))))
+        return tempSection
+
     def pure(self):
         for note in range(self.length):
             note = self.notes[randrange(len(self.notes))]
             octave = self.octaves[randrange(len(self.octaves))]
             self.notes.append(note + str(octave))
 
+    def permute(self, prefix, k):
+        n = len(self.notes)
+        #base case    
+        if (k == 0):
+            self.allBars.append(prefix)
+            return
+
+        #creates tree
+        for i in range(n):
+            newPrefix = prefix + self.notes[i]
+            self.permute(newPrefix, k-1)
 
 
